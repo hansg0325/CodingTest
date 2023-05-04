4e427474efff9d26636379dcda89cdca743e5211
stdio.han <115226460+stdio-han@users.noreply.github.com>
2023-05-04 14:21:08 +0900

    [unrated] Title: 문자열 섞기, Time: 13.57 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        String answer = "";
+        for(int i=0;i<arr1.length;i++){
+            answer+=arr1[i];
+            answer+=arr2[i];
+        }
+        return answer;
+    }
+}
