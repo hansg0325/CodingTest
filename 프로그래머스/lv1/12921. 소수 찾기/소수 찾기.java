166629ac021f2f8858b6bfb1fa7e66d200c8a436
stdio.han <115226460+stdio-han@users.noreply.github.com>
2023-05-16 15:23:56 +0900

    [level 1] Title: 소수 찾기, Time: 258.65 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean flag = true;
+		for(int i=2;i<=n;i++) {
+			flag = true;
+			for(int j=2;j<=Math.sqrt(i);j++) {
+				if(i%j==0) {
+					flag = false;
+					break;
+				}
+			}
+			if(flag==true) {
+				answer++;
+			}
+		}
+        return answer;
+    }
+}
