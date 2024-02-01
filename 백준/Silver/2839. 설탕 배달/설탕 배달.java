5fea5b8399e9e627ae70308bfd5cc8e7747755dc
HAN <hansg0325@gmail.com>
2024-02-02 00:48:30 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -8,20 +8,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
-        int count = 0;
-        while (N > 0) {
-            if (N % 5 == 0) { // N이 5로 나눠질수 있을 경우 5로 나눠버린 후 출력
-                count += N / 5; // 5로 나눈 몫을 count 에 더함
-                System.out.println(count);
-                return;
-            }
-            if (N < 3) {
-                System.out.println("-1"); // N이 3보다 적을 경우에는 -1
-                return;
-            }
-            N-=3; // N이 5로 안나눠질 경우에는 3kg 짜리 하나를 만듬
-            count++;
+
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        } else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        } else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        } else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
         }
-        System.out.println(count);
     }
 }
