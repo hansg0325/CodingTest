8576deaaaa88ba389b48446da9d0ba963b814d26
HAN <hansg0325@gmail.com>
2024-05-07 17:47:41 +0900

    [Silver III] Title: N과 M (3), Time: 568 ms, Memory: 157144 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static boolean[] checked;
+    static int[] arr;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        checked = new boolean[N + 1];
+        arr = new int[N + 1];
+        backTracking(0);
+        bw.flush();
+    }
+
+    static void backTracking(int depth) throws Exception {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+                arr[depth] = i;
+                backTracking(depth + 1);
+        }
+    }
+}
