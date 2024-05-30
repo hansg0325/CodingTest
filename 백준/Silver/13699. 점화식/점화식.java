ee4241f93f2d2524a5a0aa0f633d418dc0ad3b48
HAN <hansg0325@gmail.com>
2024-05-31 03:20:11 +0900

    [Silver IV] Title: 점화식, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1];
+
+        // 초기값
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        System.out.println(dp[n]);
+
+    }
+}
