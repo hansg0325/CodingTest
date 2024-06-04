521ad779ce2bc97c75c83491c5fd8d27d427da68
HAN <hansg0325@gmail.com>
2024-06-04 19:56:39 +0900

    [Silver II] Title: 용돈 관리, Time: 260 ms, Memory: 23168 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -27,26 +27,27 @@ public class Main {
 
             int sum = mid;
             int count = 0;
-            if (M >= getMid(mid)) {
+            if (M >= getMid(mid)) { // 인출한 횟수가 작거나 같으면 mid를 줄여야 한다
                 result = mid;
                 right = mid - 1;
-            } else {
+            } else { // 인출한 횟수가 M보다 클 경우 mid를 늘려야 한다
                 left = mid + 1;
             }
         }
     }
 
     static int getMid(int tmp) {
-        int count = 1;
-        int money = tmp;
+        //tmp = 한번에 인출할 수 있는 돈
+        int count = 1; // 돈을 인출한 횟수
+        int money = tmp; // 현재 남아있는 돈
 
-        for (int num : arr) {
-            money -= num;
-            if (money < 0) {
-                count++;
-                money = tmp - num;
+        for (int num : arr) { // 하루에 사용할 금액 num
+            money -= num; // 현재 남아있는 돈에서 사용할 금액을 뺀다
+            if (money < 0) { // 0원보다 적어지면 다시 현금을 인출한다.
+                count++; // 현금을 인출했으므로 count++
+                money = tmp - num; // 0원보다 적어질 경우 남은 금액을 통장에 넣고 다시 인출하므로
             }
         }
-        return count;
+        return count; // 인출한 횟수 리턴
     }
 }
