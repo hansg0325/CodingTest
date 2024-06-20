b8d3812fcca485968878d4c06d500e740bc5736a
HAN <hansg0325@gmail.com>
2024-06-20 16:01:16 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,67 +1,63 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int MAX = Integer.MIN_VALUE;
-    static int MIN = Integer.MAX_VALUE;
-    static int N; // 숫자 개수
-    static int[] arr; // 숫자 배열
-    static int[] op = new int[4]; // 연산자 배열
-    public static void main(String[] args) throws Exception {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int[] numbers;
+    static int[] operators = new int[4];
+    static int maxResult = Integer.MIN_VALUE;
+    static int minResult = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        // 숫자 갯수 입력
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
 
-        // 숫자 배열 입력
-        arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 연산자 갯수 입력
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            op[i] = Integer.parseInt(st.nextToken());
+            operators[i] = Integer.parseInt(st.nextToken());
         }
 
-        backTracking(arr[0], 1);
+        dfs(1, numbers[0]);
 
-        //최대값 최소값 출력
-        bw.write(MAX + "\n" + MIN);
-
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(maxResult);
+        System.out.println(minResult);
     }
 
-    // 로직
-    public static void backTracking(int num, int idx) {
-
-        // idx == N 이 되면 종료, arr 끝까지 탐색했다는 의미
-        if (idx == N) {
-            MAX = Math.max(MAX, num);
-            MIN = Math.min(MIN, num);
+    public static void dfs(int index, int currentResult) {
+        if (index == N) {
+            maxResult = Math.max(maxResult, currentResult);
+            minResult = Math.min(minResult, currentResult);
             return;
         }
 
-        for (int i = 0; i < 4; i++) {
-            // 해당 연산자가 존재하면, 해당연산자 사용한 후 개수 하나 제거
-            if (op[i] > 0) {
-                op[i]--;
-                switch (i) {
-                    case 0:	backTracking(num + arr[idx], idx + 1);	break;
-                    case 1:	backTracking(num - arr[idx], idx + 1);	break;
-                    case 2:	backTracking(num * arr[idx], idx + 1);	break;
-                    case 3:	backTracking(num / arr[idx], idx + 1);	break;
-                }
-                // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
-                op[i]++;
-            }
+        if (operators[0] > 0) {
+            operators[0]--;
+            dfs(index + 1, currentResult + numbers[index]);
+            operators[0]++;
+        }
+        if (operators[1] > 0) {
+            operators[1]--;
+            dfs(index + 1, currentResult - numbers[index]);
+            operators[1]++;
+        }
+        if (operators[2] > 0) {
+            operators[2]--;
+            dfs(index + 1, currentResult * numbers[index]);
+            operators[2]++;
+        }
+        if (operators[3] > 0) {
+            operators[3]--;
+            dfs(index + 1, currentResult / numbers[index]);
+            operators[3]++;
         }
     }
 }
