199390e0b36f8594b4424c354b97bb809fe9d4d7
HAN <hansg0325@gmail.com>
2024-08-07 11:19:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 366.55 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+      String answer = "";
+
         Arrays.sort(participant);
         Arrays.sort(completion);
-        
-        int j = 0;
-        for(int i=0;i<completion.length;i++){
+
+        for (int i = 0; i < completion.length; i++) {
             if(!participant[i].equals(completion[i])){
-                return participant[i];
+                answer = participant[i];
+                break;
             }
         }
-        return participant[participant.length - 1];
+        if(answer == "") {
+            answer = participant[participant.length-1];
+        }
+        return answer;
     }
 }
