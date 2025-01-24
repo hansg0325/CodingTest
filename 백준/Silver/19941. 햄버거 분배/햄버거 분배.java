6726a61ac51cdf23fc10a4a11220d62f25346a15
HAN <hansg0325@gmail.com>
2025-01-24 11:34:58 +0900

    [Silver III] Title: 햄버거 분배, Time: 120 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -2,29 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        char[] arr = br.readLine().toCharArray();
-        int count = 0;
+        String input = br.readLine();
+        char[] charArray = input.toCharArray();
+        boolean[] boolArray = new boolean[N];
+        int answer = 0;
+
         for (int i = 0; i < N; i++) {
-            if (arr[i] == 'P') { // 사람일 경우
-                int startIndex = Math.max(i - K, 0); // 0보다 작으면 안됨
-                int endIndex = Math.min(i + K, N - 1); // 배열의 끝을 넘어가면 안됨
-                for (int j = startIndex; j <= endIndex; j++) {
-                    if (arr[j] == 'H') {
-                        count++;
-                        arr[j] = 'X'; // 햄버거가 있다면 햄버거자리를 임의의 문자로 대체
-                        break;
+            if (charArray[i] == 'P') {
+                for (int j = 0; j < 2 * K + 1; j++) {
+                    int idx = (K * -1) + j;
+                    if (i + idx >= 0 && i + idx < N && charArray[i + idx] == 'H') {
+                        if (!boolArray[i + idx]) {
+                            boolArray[i + idx] = true;
+                            answer++;
+                            break;
+                        }
                     }
                 }
             }
-
         }
-        System.out.println(count);
+        System.out.println(answer);
+
     }
 }
