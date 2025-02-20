cb6e7add981e912c64745e1a67cf07aa3278f9c0
HAN <hansg0325@gmail.com>
2025-02-20 10:36:56 +0900

    [Gold IV] Title: 좋다, Time: 144 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,41 +1,40 @@
-import org.w3c.dom.html.HTMLParagraphElement;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        int answer = 0;
 
+        int answer = 0;
         for (int i = 0; i < N; i++) {
             int left = 0;
             int right = N - 1;
-            int target = arr[i];
             while (left < right) {
-                int sum = arr[left] + arr[right];
-                if (sum == target) { // sum이 target과 같을 경우
-                    if (left != i && right != i) { // left와 right가 같은 수가 아닐경우
+                int mid = arr[left] + arr[right];
+                if (mid == arr[i]) {
+                    if (left != i && right != i) {
                         answer++;
                         break;
-                    } else if (left == i) { 
+                    } else if (left == i) {
                         left++;
                     } else {
                         right--;
                     }
-                } else if (sum < target) { //sum이 target보다 작을 경우
-                    left++;
-                } else { // sum이 target보다 클 경우
+                } else if (mid > arr[i]) {
                     right--;
+                } else {
+                    left++;
                 }
             }
         }
