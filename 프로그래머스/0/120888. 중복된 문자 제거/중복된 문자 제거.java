4b7f6e47091b7c7a4906f427457beedb86963261
HAN <hansg0325@gmail.com>
2025-07-18 17:21:42 +0900

    [level 0] Title: 중복된 문자 제거, Time: 13.93 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        Set<Character> set = new LinkedHashSet<>();
+        for(char c : my_string.toCharArray()){
+            if(!set.contains(c)){
+                set.add(c);
+                answer+=c;
+            }
+        }
+        return answer;
+    }
+}
